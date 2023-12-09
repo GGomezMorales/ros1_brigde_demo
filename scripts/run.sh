3492d28ef1259b0de43bdd4551e30e823dad0b4a
GGomezMorales <gugomog3@gmail.com>
2023-12-09 14:30:36 -0500

    chore: merge run files

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
+source ${PROJECT_ROOT}/config.sh
+
+docker run -itd --name $CONTAINER_NAME_ROS1 \
+    --network $ROS_NETWORK \
+    -- ${DOCKER_IMAGE_NAME_ROS1}
+
+docker run -itd --name $CONTAINER_NAME_ROS2 \
+    --network $ROS_NETWORK \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -- ${DOCKER_IMAGE_NAME_ROS2}
--- scripts/run_ros1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
-source ${PROJECT_ROOT}/config.sh
-
-docker run -itd --name $CONTAINER_NAME_ROS1 --network $ROS_NETWORK ros1:noetic
