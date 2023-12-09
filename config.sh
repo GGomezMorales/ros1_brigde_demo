d4ab15e2fa3873d88306d198ca24b0496220e12b
GGomezMorales <gugomog3@gmail.com>
2023-12-09 13:23:21 -0500

    chore: add and change env variables

--- config.sh
+++ config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DOCKER_IMAGE_NAME_ROS1="ros1:noetic"
+DOCKER_IMAGE_NAME_ROS2="ros2:foxy"
 CONTAINER_NAME_ROS1="ros1_container"
 CONTAINER_NAME_ROS2="ros2_container"
+ROS1_DISTRO="noetic"
+ROS2_DISTRO="foxy"
 ROS_NETWORK="host"
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 CURRENT_DIR="$(pwd)"
 
 PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
+source ${PROJECT_ROOT}/config.sh
 
 cd "$PROJECT_ROOT/ros1"
-docker build -t ros1:noetic .
+docker build -t ${DOCKER_IMAGE_NAME_ROS1} .
 
 cd "$CURRENT_DIR"
 
 cd "$PROJECT_ROOT/ros2"
-docker build -t ros2:foxy .
+docker build -t ${DOCKER_IMAGE_NAME_ROS2} .
 
 cd "$CURRENT_DIR"
