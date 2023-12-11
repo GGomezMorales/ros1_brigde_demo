49c72e9e4d2bd4071a853136f0d3b57645e20dd5
GGomezMorales <gugomog3@gmail.com>
2023-12-10 19:38:05 -0500

    chore: build improvement

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,6 @@ CURRENT_DIR="$(pwd)"
 PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
 source ${PROJECT_ROOT}/config.sh
 
-cd "$PROJECT_ROOT/ros1"
-docker build -t ${DOCKER_IMAGE_NAME_ROS1} .
+cd "$PROJECT_ROOT"
+docker build -t ${DOCKER_IMAGE_NAME} .
 
-cd "$CURRENT_DIR"
-
-cd "$PROJECT_ROOT/ros2"
-docker build -t ${DOCKER_IMAGE_NAME_ROS2} .
-
-cd "$CURRENT_DIR"
