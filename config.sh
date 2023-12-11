7c6b29c2ea9be0cc32ae4b593c9d2bf4b1497a19
GGomezMorales <gugomog3@gmail.com>
2023-12-10 19:34:03 -0500

    chore: delete and change env variables

--- config.sh
+++ config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-DOCKER_IMAGE_NAME_ROS1="ros1:noetic"
-DOCKER_IMAGE_NAME_ROS2="ros2:foxy"
-CONTAINER_NAME_ROS1="ros1_container"
-CONTAINER_NAME_ROS2="ros2_container"
-ROS1_DISTRO="noetic"
-ROS2_DISTRO="foxy"
-ROS_NETWORK="host"
+DOCKER_IMAGE_NAME="ros1_brigde"
+CONTAINER_NAME="ros1_bridge_container"
+ROS_NETWORK="bridge"
