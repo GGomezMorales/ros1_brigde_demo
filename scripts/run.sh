1f484eec9fb3512956021bb9c7201e120028216b
GGomezMorales <gugomog3@gmail.com>
2023-12-10 19:39:43 -0500

    chore: run improvement

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,14 +3,17 @@
 PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
 source ${PROJECT_ROOT}/config.sh
 
-docker run -itd \
-    --name $CONTAINER_NAME_ROS1 \
-    --network $ROS_NETWORK \
-    ${DOCKER_IMAGE_NAME_ROS1}
 
-docker run -itd \
-    --name $CONTAINER_NAME_ROS2 \
-    --network $ROS_NETWORK \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    ${DOCKER_IMAGE_NAME_ROS2}
+xhost +local:root 
+docker container prune -f 
+docker run --privileged --rm -it \
+    --name $CONTAINER_NAME \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$XAUTH:$XAUTH" \
+    -v /dev:/dev \
+    -v $(pwd)/ros1_ws/src:/home/ros1_ws/src \
+    -v $(pwd)/ros2_ws/src:/home/ros2_ws/src \
+    -t \
+    ${DOCKER_IMAGE_NAME} 
