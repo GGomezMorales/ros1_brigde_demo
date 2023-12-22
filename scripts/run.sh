73666585f3dd44066601ca4ad95ac34275738b85
GGomezMorales <gugomog3@gmail.com>
2023-12-22 14:04:45 -0500

    chore: remove docker volumes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,6 @@
 PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
 source ${PROJECT_ROOT}/config.sh
 
-
 xhost +local:root 
 docker container prune -f 
 docker run --privileged --rm -it \
@@ -12,8 +11,5 @@ docker run --privileged --rm -it \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$XAUTH:$XAUTH" \
-    -v /dev:/dev \
-    -v $(pwd)/ros1_ws/src:/home/ros1_ws/src \
-    -v $(pwd)/ros2_ws/src:/home/ros2_ws/src \
     -t \
     ${DOCKER_IMAGE_NAME} 
